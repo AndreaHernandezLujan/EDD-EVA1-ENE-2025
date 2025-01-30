d6bb0453b799aba729106eb46eb182e33ce4c00e
AndreaHernandezLujan <159495006+AndreaHernandezLujan@users.noreply.github.com>
2025-01-30 17:44:16 -0600

    Add files via upload

---
+++ EVA1_1_MANEJO_MEMORIA/src/eva1_1_manejo_memoria/EVA1_1_MANEJO_MEMORIA.java
@@ -0,0 +1,24 @@
+
+package eva1_1_manejo_memoria;
+
+
+public class EVA1_1_MANEJO_MEMORIA {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("INICIA MAIN");
+        A();
+        System.out.println("TERMINA MAIN");
+    }
+    public static void A(){
+        System.out.println("INICIA A");
+        B();
+        System.out.println("TERMINA A");
+    }
+    public static void B(){
+        System.out.println("INICIA B");
+        System.out.println("TERMINA B");
+    }
+    
+}
