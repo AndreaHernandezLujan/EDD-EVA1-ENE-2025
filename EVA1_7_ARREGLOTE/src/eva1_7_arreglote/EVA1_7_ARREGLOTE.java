9d5b69074d75696c892f89d7c20ce5c0c45827ee
AndreaHernandezLujan <159495006+AndreaHernandezLujan@users.noreply.github.com>
2025-02-05 17:56:30 -0600

    Add files via upload

---
+++ EVA1_7_ARREGLOTE/src/eva1_7_arreglote/EVA1_7_ARREGLOTE.java
@@ -0,0 +1,26 @@
+
+package eva1_7_arreglote;
+
+/**
+ *
+ * @author andyl
+ */
+public class EVA1_7_ARREGLOTE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int datos[]= new int[100000000];
+    
+        for (int i = 0; i < datos.length; i++){    
+            datos[i]= (int) (Math.random()* 100);//Se esta llenando con valores aleatorios
+        }
+        
+    }
+    
+}
+    
+    
+
