f1e7d92286e7b64dd96d5ccc0119a8135263c2e2
AndreaHernandezLujan <159495006+AndreaHernandezLujan@users.noreply.github.com>
2025-02-04 22:30:53 -0600

    Add files via upload

---
+++ EVA1_4_COMPARAR_CADENAS/src/eva1_4_comparar_cadenas/EVA1_4_COMPARAR_CADENAS.java
@@ -0,0 +1,24 @@
+
+package eva1_4_comparar_cadenas;
+
+/**
+ *
+ * @author andyl
+ */
+public class EVA1_4_COMPARAR_CADENAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String cade1 = "Hola";
+        String cade2 = "Hola";
+        if(cade1 == cade2)
+         System.out.println("Cadenas iguales");
+         else
+         System.out.println("Cadenas diferentes");
+    
+    }
+    
+}
