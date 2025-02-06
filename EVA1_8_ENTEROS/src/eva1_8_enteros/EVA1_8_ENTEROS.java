615e3dbb7bb0d28a9197b1718186b9541ab2dcdb
AndreaHernandezLujan <159495006+AndreaHernandezLujan@users.noreply.github.com>
2025-02-05 18:01:29 -0600

    Add files via upload

---
+++ EVA1_8_ENTEROS/src/eva1_8_enteros/EVA1_8_ENTEROS.java
@@ -0,0 +1,19 @@
+
+package eva1_8_enteros;
+
+/**
+ *
+ * @author andyl
+ */
+public class EVA1_8_ENTEROS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+    }
+    
+}
